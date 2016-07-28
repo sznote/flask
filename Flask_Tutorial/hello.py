8255641eb0b3ccf55723383484a01b287d6d8e2b
saza_thi@hotmail.com <saza_thi@hotmail.com>
2016-07-28 14:27:31 +0700

    flash

--- Flask_Tutorial/hello.py
+++ Flask_Tutorial/hello.py
@@ -1,18 +1,30 @@
-from  flask import  Flask
+from  flask import  Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    x = 100 + 100
+    return "Good bye World!"
 
 @app.route("/list")
 def list():
     return "List products"
     
-@app.route('/detail/<name>')
-def detail(name):
-    return "Hello" + name + "!!"
-    
+@app.route('/user/<username>')
+def user(username):
+    return  render_template('profile.html',
+                             name=username)
+@app.route("/lotsofdata")
+def people():
+    my_people = {'Alice': 25,
+                 'Bob': 21,
+                 'Charlie': 20,
+                 'Doug': 28}
+                 
+    return jsonify(my_people)
+                
+
+                             
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     
