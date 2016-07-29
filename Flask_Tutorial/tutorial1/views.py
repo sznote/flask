c982cfcbee7075821f72b62fac5325075bee6d8f
saza_thi@hotmail.com <saza_thi@hotmail.com>
2016-07-29 23:27:22 +0700

    flask

--- Flask_Tutorial/tutorial1/views.py
+++ Flask_Tutorial/tutorial1/views.py
@@ -1,10 +1,23 @@
 from  tutorial1 import tutor1
+from  flask import request
 #from  . import tutorial1
 
 @tutor1.route('/')
 def index():
-    return "hello tutorial1"
-   
-@tutor1.route('/fuck')  
-def fuck():  
-    return "fuck"
+    return "Method userd: %s" % request.method
+ 
+@tutor1.route('/tuna')
+def tuna():
+    return '<h2>Tuna is good</h2>'
+
+@tutor1.route('/profile/<username>')
+def profile(username):
+    return "<h2>Hey There %s <h2>" % username
+
+@tutor1.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "<h2>Post ID is %s <h2>" % post_id    
+    
+@tutor1.route('/about')  
+def about():  
+    return "about"
