88bc804b6514be7e1dec69061d7f03caf58f5fb3
saza_thi@hotmail.com <saza_thi@hotmail.com>
2016-08-11 11:42:19 +0700

    flask

--- Flask_Tutorial/tutorial3/app.py
+++ Flask_Tutorial/tutorial3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,7 +6,9 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def  home():
-	return "Hello World !!!"
+	#return "Hello World !!!"
+	return render_template("home.html")
+
 
 if __name__ == "__main__":
 	app.run()
