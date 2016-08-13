98ee2d3731d55c479b8aac5a1f8d14fd69e75a4f
sahai <saza_thi@hotmail.com>
2016-08-13 22:46:44 +0700

    flask

--- Flask_Tutorial/tutorial3/app.py
+++ Flask_Tutorial/tutorial3/app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, url_for, redirect,  url_for, request
+from flask import Flask, render_template, url_for, redirect,  url_for, request, session, flash
+
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = "myname is sahai"
+
+
 
 @app.route("/")
-def  home():
+def  index():
 	#return "Hello World !!!"
-	return render_template("home.html")
+	try: 
+		mylogin = session['logined_in']
+		if mylogin == True:
+			mesg = "Login Sucess!!"
+	except:
+		mesg = "Not Login!"
+	
+	return render_template("home.html", mesg=mesg)
+
+@app.route("/home")
+def home():
+		return render_template("index.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,9 +32,16 @@ def login():
 			error = 'Invalid credentials Plases try again.'
 			print request.form['username']
 		else:
+			session['logined_in'] = True
+			flash('You ware just logged in!!')
 			return redirect(url_for('home'))
 	return render_template("login.html",error=error)
 
+@app.route('/logout')
+def logout():
+	session.pop('logined_in',None)
+	flash('You ware just logged in!!')
+	return  redirect(url_for('home'))
 
 if __name__ == "__main__":
 	app.run()
