3e84d1899f560e3fc3be14394740a2568369a06a
sahai <saza_thi@hotmail.com>
2016-08-13 13:38:16 +0700

    flask

--- Flask_Tutorial/tutorial3/app.py
+++ Flask_Tutorial/tutorial3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect,  url_for, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,17 @@ app.config['DEBUG'] = True
 def  home():
 	#return "Hello World !!!"
 	return render_template("home.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method ==  'POST':
+		if request.form['username'] != 'admin' or  request.form['password'] != 'admin':
+			error = 'Invalid credentials Plases try again.'
+			print request.form['username']
+		else:
+			return redirect(url_for('home'))
+	return render_template("login.html",error=error)
 
 
 if __name__ == "__main__":
--- Flask_Tutorial/tutorial3/readme.txt
+++ Flask_Tutorial/tutorial3/readme.txt
@@ -1,3 +1,4 @@
 1 Setting up a static site, Hello world
+2. create login page
 
 https://www.youtube.com/watch?v=WfpFUmV1d0w&list=PLLjmbh6XPGK4ISY747FUHXEl9lBxre4mM
