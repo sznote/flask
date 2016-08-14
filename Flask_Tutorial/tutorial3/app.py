4101ab569eed77862d691e0f201db265f47144ae
sahai <saza_thi@hotmail.com>
2016-08-14 22:44:15 +0700

    flask

--- Flask_Tutorial/tutorial3/app.py
+++ Flask_Tutorial/tutorial3/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect,  url_for, request, session, flash
-
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -8,20 +8,33 @@ app.config['SECRET_KEY'] = "myname is sahai"
 
 
 
-@app.route("/")
-def  index():
-	#return "Hello World !!!"
-	try: 
-		mylogin = session['logined_in']
-		if mylogin == True:
-			mesg = "Login Sucess!!"
-	except:
-		mesg = "Not Login!"
-	
-	return render_template("home.html", mesg=mesg)
+def login_required(f):
+	@wraps(f)
+	def warp(*args, **kwargs):
+		if 'logined_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash ('@Loing_required You need to login  first.')
+			return  redirect(url_for('login'))
+	return warp
 
-@app.route("/home")
+# @app.route("/")
+# def  index():
+# 	#return "Hello World !!!"
+# 	try: 
+# 		mylogin = session['logined_in']
+# 		if mylogin == True:
+# 			mesg = "Login Sucess!!"
+# 	except:
+# 		mesg = "Not Login!"
+	
+# 	return render_template("index.html", mesg=mesg)
+
+@app.route("/")
+@login_required
 def home():
+		# if  session['logined_in'] == True:
+		# 	flash("hello")	
 		return render_template("index.html")
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -33,15 +46,21 @@ def login():
 			print request.form['username']
 		else:
 			session['logined_in'] = True
-			flash('You ware just logged in!!')
+			flash('You are login Success!!')
 			return redirect(url_for('home'))
 	return render_template("login.html",error=error)
 
+@app.route('/welcome')
+def welcome():
+	return render_template('welcome.html')
+
 @app.route('/logout')
+@login_required
 def logout():
 	session.pop('logined_in',None)
-	flash('You ware just logged in!!')
-	return  redirect(url_for('home'))
+	flash('You ware just logged out!!')
+	#flash('fuck!!!!')
+	return  redirect(url_for('welcome'))
 
 if __name__ == "__main__":
 	app.run()
--- Flask_Tutorial/tutorial3/readme.txt
+++ Flask_Tutorial/tutorial3/readme.txt
@@ -2,3 +2,5 @@
 2. create login page
 
 https://www.youtube.com/watch?v=WfpFUmV1d0w&list=PLLjmbh6XPGK4ISY747FUHXEl9lBxre4mM
+
+4. Template Inheritance.
