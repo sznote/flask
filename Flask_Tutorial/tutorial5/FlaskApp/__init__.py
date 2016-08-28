4b760ec4231f5b82b5e96bc2de8c6bcaa218902d
sahai <saza_thi@hotmail.com>
2016-08-28 22:21:03 +0700

    flask

--- Flask_Tutorial/tutorial5/FlaskApp/__init__.py
+++ Flask_Tutorial/tutorial5/FlaskApp/__init__.py
@@ -1,10 +1,17 @@
 from flask import  Flask, render_template
+from  content_managemant import Content
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
 	#return "Hi there, how ya doing?"
 	return render_template('main.html')
+
+@app.route('/dashboard/')
+def dashboard():
+	#return "Hi there, how ya doing?"
+	TOPIC_DICT = Content()
+	return render_template('dashboard.html', TOPIC_DICT= TOPIC_DICT )
 
 if __name__=='__main__':
 	app.run(debug=True)
--- Flask_Tutorial/tutorial5/readme.txt
+++ Flask_Tutorial/tutorial5/readme.txt
@@ -16,3 +16,9 @@
  </VirutalHost>
 
  3. Bootstrap and jinja Templates
+ 4. Staring The Homepage
+ 5. Home page  improvements
+ 6. Finishing Homepage
+ 7. Dynamic Dashboard
+ 8. Content Manamegment
+ 9. Error  handing
