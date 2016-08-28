c6ebfe6d33c0168c1fe8624e30c197ecf1950012
sahai <saza_thi@hotmail.com>
2016-08-28 23:29:52 +0700

    flask

--- Flask_Tutorial/tutorial5/FlaskApp/__init__.py
+++ Flask_Tutorial/tutorial5/FlaskApp/__init__.py
@@ -1,6 +1,8 @@
-from flask import  Flask, render_template
+from flask import  Flask, render_template, flash
 from  content_managemant import Content
 app = Flask(__name__)
+
+app.secret_key = 'some_secret'
 
 @app.route('/')
 def homepage():
@@ -11,7 +13,31 @@ def homepage():
 def dashboard():
 	#return "Hi there, how ya doing?"
 	TOPIC_DICT = Content()
+	flash ("flash test !!!")
+	flash ("flash test1 !!!")
+	flash ("flash test2 !!!")
+
 	return render_template('dashboard.html', TOPIC_DICT= TOPIC_DICT )
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+# 	return render_template('404.html'), 404
+
+
+
+@app.route('/slashboard/')
+def slashboard():
+	#return "Hi there, how ya doing?"
+	TOPIC_DICT = Content()
+	try:
+		return render_template('dashboard.html', TOPIC_DICT= WOOP_DICT )
+	except Exception as e:
+		return render_template('505.html', error=e )
 
 if __name__=='__main__':
 	app.run(debug=True)
