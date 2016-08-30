4d913f7ef53180ea1565f91c763107754ea043e4
saza_thi@hotmail.com <saza_thi@hotmail.com>
2016-08-30 16:01:59 +0700

    flask

--- Flask_Tutorial/tutorial5/FlaskApp/__init__.py
+++ Flask_Tutorial/tutorial5/FlaskApp/__init__.py
@@ -36,16 +36,16 @@ def login_page():
         if request.method == "POST":
             attempted_username = request.form['username']
             attempted_password = request.form['password']
-            flash(attempted_username)
-            flash(attempted_password)
+            #flash(attempted_username)
+            #flash(attempted_password)
             if attempted_username == "admin" and attempted_password == "password":
                 return redirect(url_for('dashboard'))
             else:
                 error = "Invalid credential. Try Agian."
-        return render_template("login.html")
+        return render_template("login.html",error=error)
 
     except Exception as e:
-        flash(e)
+        #flash(e)
         return render_template("login.html",error=error)
 
         # if request.method == 'POST':
