d0deb4c6474bbe1d2fa1369d1bc4835ae0cf0bbf
sahai <saza_thi@hotmail.com>
2016-09-18 22:23:34 +0700

    flask

---
+++ Dis_Flask/flask-intro/app.py
@@ -0,0 +1,92 @@
+from flask import  Flask, render_template, request, url_for, redirect, session, flash, g
+from functools import wraps
+import sqlite3
+import gc
+
+app = Flask(__name__)
+app.secret_key = 'where some key'
+app.database = "sample.db"
+
+def login_required(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('You need to login frist.')
+			return  redirect(url_for('login'))
+	return wrap
+
+
+@app.route('/')
+@login_required
+def home():
+	#return "hello world!!"
+	g.db = connect_db()
+	cur =  g.db.cursor()
+	cur = g.db.execute('select * from posts')
+
+	post_dict = {}
+	posts = []
+	for row in cur.fetchall():
+		#post_dict['tilte'] =  row[0]
+		#post_ditc['description'] = row[1]
+		#posts.append(post_dict)
+		posts.append(dict(title=row[0], description=row[1]) )
+
+
+	#posts = [dict(title=row[0],description=row[1]) for row in cur.fetchall()]
+	context =   { 'title': "Login Page",
+				  'creator': "Sahai Srichock",
+				}
+	g.db.close
+	gc.collect()
+	return  render_template("index.html", posts=posts, context=context)
+
+
+
+@app.route('/welcome')
+def fwelcome():
+	#return "Welcome !!"
+	return render_template("welcome.html")
+
+@app.route('/show')
+def show():
+	name = request.args.get('name')
+	if name is not None:	
+		return name
+	else:
+		return "Hi! Guest" 
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid credentials. please try again.'
+		else:
+			session['logged_in'] = True
+			flash ('You were just logged in!')
+			return redirect(url_for('home'))
+	return  render_template("login.html", error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+	if  "logged_in" in session:
+		print session['logged_in']
+	flash('You were just logged out !!')	
+	session.pop('logged_in', None)
+	#print url_for('fwelcome') // retrun  /wecome
+	return redirect(url_for('fwelcome'))
+
+
+def connect_db():
+	return sqlite3.connect(app.database)
+
+
+if __name__ ==  '__main__':
+	app.run(debug=True)
---
+++ Dis_Flask/readme.txt
@@ -0,0 +1,65 @@
+https://github.com/realpython/
+http://www.youtube.com/playlist?list=PLLjmbh6XPGK4ISY747FUHXEl9lBxre4mM
+
+
+1. setting up a static site, hello world.
+	.
+	├── flask-intro
+	│   ├── app.py
+	│   ├── static
+	│   └── templates
+
+	python app.py
+
+
+2. Create login page
+3. User Authentication.
+	from functools import wrap
+4. Template Inheritance
+5. Databases
+	posts = [dict(title=row[0],description=row[1]) for row in cur.fetchall()]
+
+	x =  (('a1','a2'),('b1','b2'),('c1','c2'))
+	posts=[dict(t=row[0],d=row[1])  for  row in x ]
+	[{'d': 'a2', 't': 'a1'}, {'d': 'b2', 't': 'b1'}, {'d': 'c2', 't': 'c1'}]
+
+	for z in posts:
+	    print z['d']
+
+	# 
+	x={ 'a': 'hello', 'b': 'bello', 'c': 'cello' }
+	>>> x.keys()
+	['a', 'c', 'b']
+    
+    >>> x.values()
+	['hello', 'cello', 'bello']
+	
+	>>> for v in x:
+	...     print "%s:%s" %( v, x[v])
+	...
+	a:hello
+	c:cello
+	b:bello
+
+6. List Comprehensions
+	>>g.append( {'k':1,'y':2 } )
+	>>g.append( {'k':3,'y':4 } )
+	>>> g
+	[{'y': 2, 'k': 1}, {'y': 4, 'k': 3}, {'y': 2, 'k': 1}]
+	>>> for x in g:
+		print x['y']
+
+	post_dict = {}
+	posts = []
+	for row in cur.fetchall()
+		post_dict['tilte'] =  row[0]
+		post_ditc['description'] = row[1]
+		posts.append(post_dict)
+
+7. Unit Tests
+
+
+
+
+
+
