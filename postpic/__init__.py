603e6431e0eb998c6fc74ae74422714519146b58
sznote <saza_thi@hotmail.com>
2016-10-11 00:08:12 +0700

    flask

--- postpic/__init__.py
+++ postpic/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_uploads import UploadSet, UploadNotAllowed, configure_uploads, IMAGES
 from flask_sqlalchemy import SQLAlchemy
 import os 
 
@@ -9,10 +10,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "Hello saza"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///picname.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOADED_IMAGES_DEST'] =   os.path.join(BASE_DIR, "/static/images")
+app.config['UPLOADED_IMAGES_DEST'] =   os.path.join(os.path.abspath(BASE_DIR), "static/images")
 app.config['UPLOADED_IMAGES_URL'] = '/static/images/'
 app.config['DEBUG'] = True
 
+
+
+upload_images = UploadSet('images', IMAGES )
+configure_uploads(app, upload_images)
 
 
 db = SQLAlchemy(app)
